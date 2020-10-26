3f4196966ba3cb65e238c11e2b9d7f45c607baf0
Umesh Yeware <umesh.yeware@outlook.com>
2020-10-26 22:20:48 +0530

    Revert "Checked in all changes"
    This reverts commit 3b315063051c4a71830dc12f67dc9dc0b28bf7e8.

--- MarketingSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Campaigns.API
-{
-    public class MarketingSettings
-    {
-        public string ConnectionString { get; set; }      
-        public string CampaignDetailFunctionUri { get; set; }
-       
-    }
-}
