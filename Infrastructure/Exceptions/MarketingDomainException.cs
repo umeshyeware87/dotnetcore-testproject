9ed8d7152970a9273967e34cf4d9df853f0fa3f8
Umesh Yeware <umesh.yeware@outlook.com>
2020-10-26 22:29:08 +0530

    Changes Service

---
+++ Infrastructure/Exceptions/MarketingDomainException.cs
@@ -0,0 +1,21 @@
+﻿namespace Campaigns.API.Infrastructure.Exceptions
+{
+    using System;
+
+    /// <summary>
+    /// Exception type for app exceptions
+    /// </summary>
+    public class MarketingDomainException : Exception
+    {
+        public MarketingDomainException()
+        { }
+
+        public MarketingDomainException(string message)
+            : base(message)
+        { }
+
+        public MarketingDomainException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
---
+++ Infrastructure/MarketingContext.cs
@@ -0,0 +1,44 @@
+﻿namespace Campaigns.API.Infrastructure
+{
+    using System;
+    using System.IO;
+    using EntityConfigurations;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Design;
+    using Campaigns.API.Model;
+    using Microsoft.Extensions.Configuration;
+
+    public class MarketingContext : DbContext
+    {
+        public MarketingContext(DbContextOptions<MarketingContext> options) : base(options)
+        {    
+        }
+
+        public DbSet<Campaign> Campaigns { get; set; }
+
+        public DbSet<Rule> Rules { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new CampaignEntityTypeConfiguration());
+           
+        }
+    }
+
+    public class MarketingContextDesignFactory : IDesignTimeDbContextFactory<MarketingContext>
+    {
+        public MarketingContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var connectionString = configuration["ConnectionString"];
+            var optionsBuilder = new DbContextOptionsBuilder<MarketingContext>()
+                .UseSqlServer(connectionString);
+            return new MarketingContext(optionsBuilder.Options);
+        }
+    }
+}
